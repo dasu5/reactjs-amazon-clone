81689205cf07250a2d612ea11749c614f8c09206
dasu5 <dasunianupama@gmail.com>
2020-09-18 22:56:45 +0530

    completed home page added

--- src/Home.js
+++ src/Home.js
@@ -8,28 +8,56 @@ function Home() {
       <div className="home__container">
         <img
           className="home__image"
-          src="https://m.media-amazon.com/images/G/01/digital/video/sonata/Superhero_UK_Acquisition_FT_Apr_20/7a2ef2c8-d54c-4da8-beba-f4e12a0f10d5._UR1280,600_SX1500_FMjpg_.jpg"
+          src="https://www.overons.kpn/images/news/_1280xAUTO_crop_center-center_none/Amazon-Prime-Video.jpg"
           alt=""
         />
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            title="PS4 Controller, PS4 Remote, PS4 Joystick, Wireless PS4 Controller Compatible for Playstation 4 (Red)"
+            price={39.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/61sGVkv5NVL._AC_SX466_.jpg"
+            rating={4}
+          />
+          <Product
+            title="The ROG Strix XG49VQ is a super ultra-wide 49” Dual full HD gaming monitor with a smooth 144Hz refresh rate that offers the ultimate immersive gaming"
+            price={50.0}
+            image="https://images-na.ssl-images-amazon.com/images/I/71PjhKA%2BllL._AC_SX466_.jpg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            title="PADY-Wearable Technology Honor Band 4 6-Axis Inertial Heart Rate Monitor. Infrared Light Wear Detection Sensor Full Touch"
+            price={25.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51B7xN1C5RL._AC_SX425_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Combatwing Gaming Headset- Xbox 360 Headset PS4 Headset PC Headset with Noice Cancelling Mic"
+            price={23.5}
+            image="https://images-na.ssl-images-amazon.com/images/I/71HFFgckuFL._AC_SL1267_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Apple watch series 5(GPS,44MM) - Space Gray Aluminium Case with Black Sport Band"
+            price={21.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71mbZF8PT1L._AC_SX569_.jpg"
+            rating={5}
+          />
         </div>
 
         <div className="home__row">
-          <Product />
+          <Product
+            title="Adding to the Amazon Echo Family: Echo Dot and Amazon Tap "
+            price={35.99}
+            image="https://miro.medium.com/max/1178/1*wG7KYb1fGEyBcWS6ohx-1Q.jpeg"
+            rating={5}
+          />
         </div>
       </div>
     </div>
   );
 }
-//https://images-eu.ssl-images-amazon.com/images/G/02/digital/
-//video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE_XSite_150x
-//600_PV_en-GB.CB428684220_.jpg
+//https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_Sy400_.jpg
 export default Home;
